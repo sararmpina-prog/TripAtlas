1a002f47174969e6f549125d22c235b3e64ed7ee
Sara Raquel Madeira Pina <sararmpina@gmail.com>
2026-06-11 14:24:45 +0100

    login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,60 @@
+import { Link } from "react-router";
+import WelcomeCard from "../components/WelcomeCard";
+
 
 function Login() {
+  return (
+    <div>
 
-    return ( 
+      <section>
+        <h1>Don't have an account yet?</h1>
+
+        <p>Sign up here:</p>
+
+        <Link to="/register">
+          <button>
+            Register
+          </button>
+            </Link>
+        </section>
+
+      <WelcomeCard>
+        <h2>Welcome Back!</h2>
+
+        <p>Your travel plans are waiting for you</p>
+
+        <form>
+          <input
+            type="email"
+            placeholder="Email"
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+          />
+
+        
         <div>
-            <h1>Don't have an account yet?</h1>
-            <p>Sign up here:</p>
-            <button>Register</button>
+            <label>
+            <input type="checkbox" />
+            Remember me
+        </label>
+
+        <Link to="/forgot-password">
+        Forgot password?
+        </Link>
         </div>
-    )
+
+
+          <button type="submit">
+            Login
+          </button>
+        </form>
+      </WelcomeCard>
+
+    </div>
+  );
 }
 
-export default Login
+export default Login;
