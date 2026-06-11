cc5cc2d47cd0bb46900b9e94013b4b4b9d1bbb4f
Sara Raquel Madeira Pina <sararmpina@gmail.com>
2026-06-11 12:33:24 +0100

    main e instalações

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider } from './context/ThemeContext'
+import { PreferencesProvider } from './context/PreferencesContext'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+      <PreferencesProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PreferencesProvider>
+    </ThemeProvider>
+    <ReactQueryDevtools  initialIsOpen={false}/>
+    </QueryClientProvider>
   </StrictMode>,
 )
