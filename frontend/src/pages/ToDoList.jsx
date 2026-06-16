9703ec117bd9ac044ffc5fe9adc3ef80fda8f7b2
Sara Raquel Madeira Pina <sararmpina@gmail.com>
2026-06-16 12:17:58 +0100

    Todo

---
+++ frontend/src/pages/ToDoList.jsx
@@ -0,0 +1,25 @@
+import '../styles/Dashboard.css';
+import {Header} from '../components/Header'
+
+
+export default function ToDoList() {
+  
+
+    return (
+        <div>
+            < Header />
+            <ul>
+                <li>Register success - New Trip/View Trips</li>
+                <li>Cards editáveis (Dashboard)</li>
+                <li>Chat Backend</li>
+                <li>Chat Ligação Frontend</li>
+                <li>Menu User</li>
+                <li>Página Editar User</li>
+                <li>Footer</li>
+                <li>Responsividade</li>
+                <li>Páginas de Erros (Opcional)</li>
+                <li>Adicionar funcionalidades (Calendário, packing list, Weather)</li>
+            </ul>
+        </div>
+    ) 
+}
