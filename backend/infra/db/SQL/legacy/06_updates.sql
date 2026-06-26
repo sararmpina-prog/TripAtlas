8a3328954d51cf77927c4a635e9dc8f50099fd19
upskill228 <upskill228@alunos.ciencias.ulisboa.pt>
2026-06-26 11:36:37 +0100

    Delete backend directory

--- backend/infra/db/SQL/legacy/06_updates.sql
+++
@@ -1,9 +0,0 @@
-/*  ******* EXEMPLOS ******* */
-
-UPDATE tarefas
-SET estado = 'concluida'
-WHERE id = 1;
-
-UPDATE utilizadores
-SET telefone = '939999999'
-WHERE id = 3;
--- backend/middlewares/asyncHandler.js
+++
@@ -1,5 +0,0 @@
-/* Apanha erros de funções async e envia para next() -> errorHandler.js */
-
-export const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
